c645d836aab5a4900e2f1e1c9a6cb93f0cd48520
Thomas Fisher <tommyfisher@gmail.com>
2015-10-19 19:41:01 -0700

    Queries

--- lib/ecto_blog/post.ex
+++ lib/ecto_blog/post.ex
@@ -15,4 +15,15 @@ defmodule EctoBlog.Post do
     |> cast(params, ~w(title), ~w(content))
     |> validate_length(:title, min: 3)
   end
+
+  def highly_rated(query) do
+    from p in query,
+      left_join: r in assoc(p, :ratings),
+      having: avg(r.value) > 4, group_by: p.id
+  end
+
+  def most_recent(query) do
+    from p in query,
+      order_by: [desc: p.inserted_at], limit: 5
+  end
 end
